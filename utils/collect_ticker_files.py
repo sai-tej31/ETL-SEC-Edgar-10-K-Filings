b208658e08abf04b3396862e9b0b7303ac9beb79
pChitral <chitral.patil@vit.edu.in>
2023-07-21 18:34:27 -0700

    updated collect_ticker_files

--- utils/collect_ticker_files.py
+++ utils/collect_ticker_files.py
@@ -1,14 +1,13 @@
 import logging
-from ticker_utils import TickerFilesCollector
+from utils.TickerFilesCollector import TickerFilesCollector
 
 
-def collect_ticker_files(data_folder, tickers):
+def collect_ticker_files(data_folder="data/sec-edgar-filings"):
     """
-    Collects and organizes ticker files from the specified data folder for the given tickers.
+    Collects and organizes ticker files from the specified data folder for all tickers.
 
     Parameters:
-        data_folder (str): The path to the root folder where company files are located.
-        tickers (list): A list of ticker symbols for which files should be collected.
+        data_folder (str, optional): The path to the root folder where company files are located. Default is 'data/sec-edgar-filings'.
 
     Returns:
         dict: A dictionary with company tickers as keys and lists of associated file paths as values.
@@ -20,14 +19,11 @@ def collect_ticker_files(data_folder, tickers):
         # Get all ticker files from the collector
         all_ticker_files = ticker_collector.get_all_ticker_files()
 
-        # Create a new dictionary with ticker symbols as keys and associated file paths as values
-        collected_files_dict = {}
-        for ticker in tickers:
-            ticker_files = all_ticker_files.get(ticker, [])
-            collected_files_dict[ticker] = ticker_files
+        # Print and return the collected files dictionary
+        for ticker, ticker_files in all_ticker_files.items():
             print(f"Files for {ticker}: {ticker_files}")
 
-        return collected_files_dict
+        return all_ticker_files
 
     except Exception as e:
         # Log the error message
@@ -43,11 +39,4 @@ if __name__ == "__main__":
         format="%(asctime)s - %(levelname)s - %(message)s",
     )
 
-    data_folder = (
-        "data/sec-edgar-filings"  # Replace this with the path to your data folder
-    )
-    tickers_to_collect = [
-        "AAPL",
-        "MSFT",
-    ]  # Replace this with the desired list of tickers
-    all_ticker_files = collect_ticker_files(data_folder, tickers_to_collect)
+    all_ticker_files = collect_ticker_files()  # Uses the default data folder
